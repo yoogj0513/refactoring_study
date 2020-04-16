436a171fd7756cdec74bb83a2cd54c8e82792d91
yoogj0513 <yoogj0513@naver.com>
2020-04-16 10:41:03 +0900

    적립포인트 임시 변수 메서드 호출로 전환

--- src/main/java/refactoring_study/Customer.java
+++ src/main/java/refactoring_study/Customer.java
@@ -21,15 +21,10 @@ public class Customer {
 	}
 	
 	public String statement() {
-		int frequentRenterPoints = 0;
-		
 		StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 		
 		//비디오 종류별 대여료 계산
 		for(Rental each : rentals) {
-			// 적립 포인트를 1포인트 증가 / 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-			frequentRenterPoints += each.getFrequentRenterPoints();
-			
 			//이번에 대여한 비디오 정보와 대여료를 출력
 			result.append("\t" + each.getMovie().getTitle());
 			result.append("\t" + String.valueOf(each.getCharge()) + "\n");
@@ -38,11 +33,21 @@ public class Customer {
 		
 		//푸터행 추가
 		result.append("누적 대여료 : " + String.valueOf(getTotalChange()) + "\n");
-		result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
+		result.append("적립 포인트 : " + String.valueOf(getTotalFrequentRenterPoints()));
 		
 		return result.toString();
 	}// end of method statement
 
+	// 적립포인트 (적립 포인트를 1포인트 증가 / 최신물을 이틀 이상 대여하면 보너스 포인트 지급)
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		for(Rental rental : rentals) {
+			result += rental.getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+	// 누적된 총 대여료
 	private double getTotalChange() {
 		double result = 0;
 		for(Rental rental : rentals) {
