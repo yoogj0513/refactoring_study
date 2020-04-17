dd9e3db900d85b7aad7d7285806b1564e1c812e8
yoogj0513 <yoogj0513@naver.com>
2020-04-17 10:11:38 +0900

    조건문을 재정의로 전환 기법

--- src/main/java/refactoring_study/Price.java
+++ src/main/java/refactoring_study/Price.java
@@ -3,26 +3,9 @@ package refactoring_study;
 public abstract class Price {
 	abstract int getPriceCode();
 	
-	public double getCharge(int daysRented) {
-		double result = 0;
-		
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if(daysRented > 2) {
-				result += (daysRented -2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if(daysRented > 3) {
-				result += (daysRented -3) * 1.5;
-			}
-			break;
-		}
-		return result;
+	abstract double getCharge(int daysRented);
+
+	public int getFrequentRenterPoints(int daysRented) {
+		return 1;
 	}
 }
